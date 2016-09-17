25bbc16466ade07912fde807027c134e3b8b92ed
Rhett Patrick G. Atienza <rhettpatrickgarciaatienza08@gmail.com>
2016-09-17 14:21:28 +0800

    Adds FileReader

--- Midterms/src/midterms/Midterms.java
+++ Midterms/src/midterms/Midterms.java
@@ -1,10 +1,29 @@
 
 package midterms;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Midterms {
   
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
       
+        
+        Scanner in = new Scanner(new FileReader("input.txt"));
+        while(in.hasNext()){
+            
+            System.out.println(in.nextLine());
+        
+        
+        
+        }
+        
+        
+        
+        
+        
+        
     }
     
 }
